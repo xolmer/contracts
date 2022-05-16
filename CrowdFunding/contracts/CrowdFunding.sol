b40a8d46ebcf9dc8fdeb6c50f37b871a73489bf3
Xolmer <xolmer@Xolmers-MacBook-Pro.local>
2022-05-16 20:33:55 +0200

    CrowdFunding Request to spend

--- CrowdFunding/contracts/CrowdFunding.sol
+++ CrowdFunding/contracts/CrowdFunding.sol
@@ -10,6 +10,18 @@ contract CrowdFunding {
     uint256 public goal;
     uint256 public raisedAmount;
 
+    struct Request {
+        string description;
+        address payable recipient;
+        uint256 value;
+        bool completed;
+        uint256 noOfVoters;
+        mapping(address => bool) voters;
+    }
+
+    mapping(uint256 => Request) public requests;
+    uint256 public numRequests;
+
     constructor(uint256 _goal, uint256 _deadline) {
         goal = _goal;
         deadline = block.timestamp + _deadline;
@@ -36,5 +48,38 @@ contract CrowdFunding {
 
     function getBalance() public view returns (uint256) {
         return address(this).balance;
+    }
+
+    function getRefund() public {
+        require(
+            block.timestamp > deadline && raisedAmount < goal,
+            "Refund is not possible"
+        );
+        require(contributors[msg.sender] > 0, "You have not contributed");
+
+        address payable recipient = payable(msg.sender);
+        uint256 value = contributors[msg.sender];
+        recipient.transfer(value);
+        contributors[msg.sender] = 0;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can perform this action");
+        _;
+    }
+
+    function createRequest(
+        string memory _description,
+        address payable _recipient,
+        uint256 _value
+    ) public onlyAdmin {
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.noOfVoters = 0;
     }
 }
